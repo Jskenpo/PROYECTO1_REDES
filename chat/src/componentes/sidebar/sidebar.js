8fbf4e11dae70d86f82e4c5e3c769bafd19a7c48
Santisteban <san21153@uvg.edu.gt>
2024-08-10 18:30:19 -0600

    login y logout mplementado

--- chat/src/componentes/sidebar/sidebar.js
+++ chat/src/componentes/sidebar/sidebar.js
@@ -2,12 +2,24 @@ import React from 'react';
 import Icon from '@mdi/react';
 import Button from 'react-bootstrap/esm/Button';
 import {  mdiMessagePlusOutline, mdiCog, mdiContacts, mdiLogout } from '@mdi/js';
+import { useNavigate } from 'react-router-dom';
+import { client } from '@xmpp/client';
+
 
 import './sidebar.css';
 
 function Sidebar() {
 
+    const navigate = useNavigate();
     
+    const handleLogout = () => {
+        const xmppClient = client();
+        xmppClient.stop();
+        console.log('🔴', 'offline');
+        navigate('/', { replace: true });
+        localStorage.removeItem('user');
+        localStorage.removeItem('password');
+    }
     
     return (
         <div id="sidebar">
@@ -20,7 +32,7 @@ function Sidebar() {
             <Button style={{ backgroundColor: "transparent", border: "none" }} >
                 <Icon path={mdiCog} size={1} color='#000000'/>
             </Button>
-            <Button style={{ backgroundColor: "transparent", border: "none" }} >
+            <Button style={{ backgroundColor: "transparent", border: "none" }} onClick={handleLogout} >
                 <Icon path={mdiLogout} size={1} color='#000000'/>
             </Button>
             
--- chat/src/paginas/login/login.js
+++ chat/src/paginas/login/login.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./login.css";
 import LoginCard from "../../componentes/LoginCard/LoginCard";
-import Sidebar from "../../componentes/sidebar/sidebar";
 
 function Login() {
     return (
     <div className="login">
-        <Sidebar />
+        <LoginCard />
     </div>
     );
     }
